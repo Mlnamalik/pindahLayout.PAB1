2a8ba1057ef8b22e8b7fdcefbd5c1dfd5c584f6b
Mlnamalik <maulanamalik_03@mhs.mdp.ac.id>
2022-10-04 19:46:04 +0700

    4-10-22

---
+++ PindahLayout-utama/app/src/main/java/com/if3b/pindahlayout/MainActivity.java
@@ -0,0 +1,35 @@
+package com.if3b.pindahlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnPindah;
+    EditText etNama;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Layout A");
+
+        btnPindah = findViewById(R.id.btn_pindah);
+        etNama = findViewById(R.id.et_nama)
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(pindah);
+            }
+        });
+
+    }
+}
